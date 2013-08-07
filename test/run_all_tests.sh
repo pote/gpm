d06d5f96c887f24c6623f25894be2d757ed144c2
PoTe <poteland@gmail.com>
2013-08-07 20:44:55 -0300

    run_all_tests should fail with a non-zero status

--- test/run_all_tests.sh
+++ test/run_all_tests.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+status=0
 echo ">> Now running all tests"
 for test in *test.sh
 do
   echo ">> Current test: $test"
-  ./$test
+  ./$test || $status=!?
 done
 echo ">> All Done"
+exit $status
