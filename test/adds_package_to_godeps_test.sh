4aaf772873e0998d078414edaa127048197d6653
PoTe <poteland@gmail.com>
2013-08-26 10:13:42 -0300

    Do not add duplicated packages

--- test/adds_package_to_godeps_test.sh
+++ test/adds_package_to_godeps_test.sh
@@ -1,10 +1,19 @@
 . assert.sh
 
+# Tests adding a new package to the Godeps file.
 touch testGodeps
 ../bin/johnny_deps -a github.com/pote/johnny-deps-testing-package -f testGodeps
 assert "echo "$?"" "0"
 assert "cat testGodeps" "github.com/pote/johnny-deps-testing-package v5.1"
+rm testgodeps
 
-# Cleanup
-rm testGodeps
+
+# Tests tring to add a duplicated package in the Godeps file
+echo "github.com/pote/johnny-deps-testing-package v5.1" > testGodeps
+../bin/johnny_deps -a github.com/pote/johnny-deps-testing-package -f testGodeps
+assert "echo "$?"" "1"
+assert "cat testGodeps" "github.com/pote/johnny-deps-testing-package v5.1"
+rm testgodeps
+
+
 assert_end examples
