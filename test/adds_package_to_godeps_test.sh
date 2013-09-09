14389f25c155d11f98d04d2b13791bf3da1e9625
PoTe <poteland@gmail.com>
2013-09-09 19:42:37 -0300

    -H flag adds HEAD to the Godeps file.
    Closes #1

--- test/adds_package_to_godeps_test.sh
+++ test/adds_package_to_godeps_test.sh
@@ -7,6 +7,13 @@ assert "echo "$?"" "0"
 assert "cat testGodeps" "github.com/pote/johnny-deps-testing-package v5.1"
 rm testgodeps
 
+# Tests adding a new package to the Godeps file using its HEAD commit.
+touch testGodeps
+../gpm -a github.com/pote/johnny-deps-testing-package -f testGodeps -H
+assert "echo "$?"" "0"
+assert "cat testGodeps" "github.com/pote/johnny-deps-testing-package  6596ffa75610ea329fb6ff13ac7da289143d427b"
+rm testgodeps
+
 
 # Tests tring to add a duplicated package in the Godeps file
 echo "github.com/pote/johnny-deps-testing-package v5.1" > testGodeps
