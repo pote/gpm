1dfd850c2a5ea37e4f962bf9f4947834b57af05c
PoTe <poteland@gmail.com>
2013-09-09 19:53:10 -0300

    Huh, weird, this should have broken the tests locally?

--- test/adds_package_to_godeps_test.sh
+++ test/adds_package_to_godeps_test.sh
@@ -5,14 +5,14 @@ touch testGodeps
 ../gpm -a github.com/pote/johnny-deps-testing-package -f testGodeps
 assert "echo "$?"" "0"
 assert "cat testGodeps" "github.com/pote/johnny-deps-testing-package v5.1"
-rm testgodeps
+rm testGodeps
 
 # Tests adding a new package to the Godeps file using its HEAD commit.
 touch testGodeps
 ../gpm -a github.com/pote/johnny-deps-testing-package -f testGodeps -H
 assert "echo "$?"" "0"
-assert "cat testGodeps" "github.com/pote/johnny-deps-testing-package  6596ffa75610ea329fb6ff13ac7da289143d427b"
-rm testgodeps
+assert "cat testGodeps" "github.com/pote/johnny-deps-testing-package  6596ffa75610ea329fb6ff13ac7da289143d427b "
+rm testGodeps
 
 
 # Tests tring to add a duplicated package in the Godeps file
@@ -20,7 +20,7 @@ echo "github.com/pote/johnny-deps-testing-package v5.1" > testGodeps
 ../gpm -a github.com/pote/johnny-deps-testing-package -f testGodeps
 assert "echo "$?"" "1"
 assert "cat testGodeps" "github.com/pote/johnny-deps-testing-package v5.1"
-rm testgodeps
+rm testGodeps
 
 
 assert_end examples
